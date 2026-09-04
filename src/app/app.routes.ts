aed70bf5b28883c35948e3f3c5e41417d47fb491
atulinbits <atul260702@gmail.com>
2026-09-04 09:41:43 +0530

    refactor: migrate from SSR to CSR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { Registration } from './pages/registration/registration';
-import { Home } from './pages/home/home';
-import { Schedule} from './pages/schedule-meeting/schedule';
-import { InstructorPage } from './pages/instructor-page/instructor-page';
-import { ConferenceRoom} from './pages/conference-room/conference-room';
 
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'registration', component: Registration },
-  { path: 'schedule', component: Schedule },
-  { path: 'instructor', component: InstructorPage},
-  { path: 'conference', component: ConferenceRoom },
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { Navbar } from './components/layout/navbar/navbar';
-import { CommonModule } from '@angular/common'; // <-- Import CommonModule
-import { FooterComponent } from './components/layout/footer/footer';
-import { filter } from 'rxjs';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet, Navbar, FooterComponent, CommonModule],
+  imports: [RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('vartahub-ui');
-  private router = inject(Router);
-  currentUrl = '';
-
-  constructor() {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .forEach((event: any) => {
-        this.currentUrl = event.urlAfterRedirects;
-      });
-  }
-
-  isConferenceRoute(): boolean {
-    return this.currentUrl.includes('/conference');
-  }
 }
