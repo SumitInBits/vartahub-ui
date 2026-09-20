2e3b4af2967c4448c4f9d1facc398036d8fce099
atulinbits <atul260702@gmail.com>
2026-09-20 23:25:03 +0530

    feat: get started with formly forms

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,6 +15,8 @@ import {
 } from 'keycloak-angular';
 import { baseUrlInterceptor } from './interceptors/base-url-interceptor';
 import { environment } from '../environments/environment.development';
+import { provideFormlyCore } from '@ngx-formly/core';
+import { withFormlyMaterial } from '@ngx-formly/material';
 
 const urlCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
   urlPattern: new RegExp(`^${environment.baseUrl}(\\/.*)?$`, 'i'),
@@ -35,5 +37,6 @@ export const appConfig: ApplicationConfig = {
       provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
       useValue: [urlCondition],
     },
+    provideFormlyCore(withFormlyMaterial()),
   ],
 };
